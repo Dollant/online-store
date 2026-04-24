af19d30062e09d1b730f71a42f94a8adcf2ef392
Dollant <ariawilliams2425@gmail.com>
2026-04-24 10:39:53 -0400

    Added while loop that keeps home screen running until user enters 3 to exit.

--- src/main/java/com/pluralsight/Store.java
+++ src/main/java/com/pluralsight/Store.java
@@ -52,5 +52,16 @@ public class Store {
 
     public static void showHomeScreen() {
         String choice = "";
+
+        while (!choice.equals("3")) {
+            System.out.println("\n°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･");
+            System.out.println("\t Welcome to Dolly Byte  ");
+            System.out.println("°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･");
+            System.out.println("1 ࣪ ִֶָ☾. Display Products");
+            System.out.println("2 ࣪ ִֶָ☾. Display Cart");
+            System.out.println("3 ࣪ ִֶָ☾. Exit");
+            System.out.print("\nChoose an option: ");
+            choice = keyboard.nextLine().trim();
+        }
     }
 }
